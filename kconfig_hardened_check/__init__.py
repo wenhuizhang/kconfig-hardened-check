30e37ba74a69aef482958dea43b5393ab6d39923
Alexander Popov <alex.popov@linux.com>
2022-03-27 23:46:11 +0300

    Add the infrastructure for cmdline checks

--- kconfig_hardened_check/__init__.py
+++ kconfig_hardened_check/__init__.py
@@ -81,7 +81,7 @@ import re
 import json
 from .__about__ import __version__
 
-TYPES_OF_CHECKS = ('kconfig', 'version')
+TYPES_OF_CHECKS = ('kconfig', 'version', 'cmdline')
 
 class OptCheck:
     def __init__(self, reason, decision, name, expected):
@@ -722,7 +722,7 @@ def populate_simple_opt_with_data(opt, data, data_type):
     if data_type != opt.type:
         return
 
-    if data_type == 'kconfig':
+    if data_type in ('kconfig', 'cmdline'):
         opt.state = data.get(opt.name, None)
     elif data_type == 'version':
         opt.ver = data
@@ -737,7 +737,7 @@ def populate_opt_with_data(opt, data, data_type):
             else:
                 populate_simple_opt_with_data(o, data, data_type)
     else:
-        if opt.type != 'kconfig':
+        if opt.type not in ('kconfig', 'cmdline'):
             sys.exit('[!] ERROR: bad type "{}" for a simple check {}'.format(opt.type, opt.name))
         populate_simple_opt_with_data(opt, data, data_type)
 
@@ -774,6 +774,13 @@ def parse_kconfig_file(parsed_options, fname):
 
             if option:
                 parsed_options[option] = value
+
+
+def parse_cmdline_file(parsed_options, fname):
+    with open(fname, 'r') as f:
+        print('FIXME! cmdline file:')
+        for line in f.readlines():
+            print(line)
 
 
 def main():
@@ -831,6 +838,15 @@ def main():
         parse_kconfig_file(parsed_kconfig_options, args.config)
         populate_with_data(config_checklist, parsed_kconfig_options, 'kconfig')
         populate_with_data(config_checklist, kernel_version, 'version')
+
+        if args.cmdline:
+            # add relevant cmdline checks to the checklist
+            add_cmdline_checks(config_checklist, arch)
+
+            # populate the checklist with the parsed kconfig data
+            parsed_cmdline_options = OrderedDict()
+            parse_cmdline_file(parsed_cmdline_options, args.cmdline)
+            populate_with_data(config_checklist, parsed_cmdline_options, 'cmdline')
 
         # now everything is ready for performing the checks
         perform_checks(config_checklist)
